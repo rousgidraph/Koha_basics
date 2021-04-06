d6157b6b71914716f789f68d65ba209f18572f3a
rousgidraph <rousgidraph@gmail.com>
2021-04-06 09:54:22 +0300

    Inital commit

---
+++ scripts/get_koha.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+#update system 
+
+#add koha repo 
+
+#install koha-common 
+
+#configure koha-conf.xml file 
+
+#configure apache ports to listen to 
+
---
+++ scripts/getkoha.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+echo "**********************"
+echo "This will set up and install koha "
+echo "**********************"
+echo "It might not be perfect, but its the thought that counts "
